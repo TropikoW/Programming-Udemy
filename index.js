e66b6dbc2369fab7b7b7511860504f6b15d5447d
TropikoW <jovannykr@hotmail.com>
2023-01-26 09:16:33 -0500

    i add the the image

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const buttonInitiate = document.querySelector('#button-initiate');
 const buttonUs = document.querySelector('#button-us');
 const buttonClient = document.querySelector('#button-client');
 const buttonContact = document.querySelector('#button-contact');
+const buttonContactFirst = document.querySelector('#contact-first');
 
 buttonInitiate.addEventListener('click',messageAlert);
 buttonUs.addEventListener('click',messageAlert);
 buttonClient.addEventListener('click',messageAlert);
 buttonContact.addEventListener('click',messageAlert);
+buttonContactFirst.addEventListener('click',messageAlert);
 
 function messageAlert() {
     alert('Hi!, we no have for the moment site for this link, please wait');
